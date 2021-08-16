58bc5756e1c8b909476b2fe369869b7ce43ceb66
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2021-08-16 11:30:35 +0530

    Add files via upload

---
+++ Problem Solving/linkedlist/InsertNodeatASpecificPosition.c
@@ -0,0 +1,113 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+typedef struct SinglyLinkedListNode SinglyLinkedListNode;
+typedef struct SinglyLinkedList SinglyLinkedList;
+
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+struct SinglyLinkedList {
+    SinglyLinkedListNode* head;
+    SinglyLinkedListNode* tail;
+};
+
+SinglyLinkedListNode* create_singly_linked_list_node(int node_data) {
+    SinglyLinkedListNode* node = malloc(sizeof(SinglyLinkedListNode));
+
+    node->data = node_data;
+    node->next = NULL;
+
+    return node;
+}
+
+void insert_node_into_singly_linked_list(SinglyLinkedList** singly_linked_list, int node_data) {
+    SinglyLinkedListNode* node = create_singly_linked_list_node(node_data);
+
+    if (!(*singly_linked_list)->head) {
+        (*singly_linked_list)->head = node;
+    } else {
+        (*singly_linked_list)->tail->next = node;
+    }
+
+    (*singly_linked_list)->tail = node;
+}
+
+void print_singly_linked_list(SinglyLinkedListNode* node, char* sep, FILE* fptr) {
+    while (node) {
+        fprintf(fptr, "%d", node->data);
+
+        node = node->next;
+
+        if (node) {
+            fprintf(fptr, "%s", sep);
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    SinglyLinkedListNode* temp = (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));
+    if(head == NULL) {
+        head->data = data;
+        head->next = NULL;
+        return head;
+    } else {
+        if(position == 0) {
+            temp->data = data;
+            temp->next = head;
+            return temp;
+        } else {
+            SinglyLinkedListNode* prev = head;
+            temp->data = data;
+            for(int i = 0; i < position-1; i++) {
+                prev = prev->next;
+            }
+            temp->next = prev->next;
+            prev->next = temp;
+        }
+    }
+    return head;
+}
+
+
+int main()
